e5c9451348429d0dab09c0ab2d534796bbf98279
Tao Feng <taofeng311@gmail.com>
2015-04-02 11:34:43 -0700

    create TodoList Collection

--- todos.js
+++ todos.js
@@ -12,4 +12,29 @@ $(function() {
 			});
 		}
 	});
+
+
+	var TodoList = Backbone.Collections.extend({
+		model: Todo,
+		localStorage: new Backbone.localStorage("todos-backbone"),
+
+		done: function(){
+			return this.where({done = true});
+		},
+
+		remaining: function() {
+			return this.where({done = false});
+		},
+
+		nextOrder: function() {
+			if(!this.length) 
+				return 1;
+			else
+				return this.last().get("order") + 1;
+		},
+
+		comparator: order
+	});
+
+	var Todos = new TodoList;
 });
