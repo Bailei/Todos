006cd903315b4fa8c88082309631534bac5235fd
Tao Feng <taofeng311@gmail.com>
2015-04-02 12:03:20 -0700

    create the TodoView

--- todos.js
+++ todos.js
@@ -14,7 +14,7 @@ $(function() {
 	});
 
 
-	var TodoList = Backbone.Collections.extend({
+	var TodoList = Backbone.Collection.extend({
 		model: Todo,
 		localStorage: new Backbone.localStorage("todos-backbone"),
 
@@ -33,8 +33,64 @@ $(function() {
 				return this.last().get("order") + 1;
 		},
 
-		comparator: order
+		comparator: 'order'
 	});
 
 	var Todos = new TodoList;
+
+	var TodoView = Backbone.View.extend({
+		tagName: "li",
+		template: _.template($('#item_template').html()),
+		events: {
+			"click .toggle": "toggleDone",
+			"dbclick .view": "edit",
+			"click a.destroy": "clear",
+			"keypress .edit": "updateOnEnter",
+			"blur .edit": "close"
+		},
+
+		initialize: function(){
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+		},
+
+		render: function(){
+			this.$el.html(template(this.model.toJSON()));
+			this.$el.toggleClass('done', this.model.get('done'));
+			this.input = this.$('.edit');
+			return this;
+		},
+
+		toggleDone: function(){
+			this.model.toggle();
+		},
+
+		edit: function(){
+			$(this.el).addClass("editing");
+			this.input.focus();
+		},
+
+		clear: function(){
+			this.model.destroy();
+		},
+
+		//close the edit form
+		close: function(){
+			var value = this.input.val();
+			if(!value){
+				this.clear();
+			}else{
+				this.model.save({title: value});
+				this.$el.removeClass("editing");
+			}
+		},
+
+		//close the edit form when click the "enter/return" button on keyboard
+		updateOnEnter: function(){
+			if(e.keyCode = 13) 
+				this.close();
+		}
+	});
+
+	
 });
