707407741847759519009a704d280f20b06bac31
Tao Feng <taofeng311@gmail.com>
2015-04-02 13:16:18 -0700

    create AppView

--- todos.js
+++ todos.js
@@ -92,5 +92,78 @@ $(function() {
 		}
 	});
 
-	
+	var AppView = Backbone.View.extend({
+		el: $("#todoapp"),
+		statsTemplate: _.template($("stats-template").html()),
+		events: {
+			"keypress #new-todo": "createOnEnter",
+			"click #clear-completed": "clearCompleted",
+			"click #toggle-all": "toggleAllCompleted"
+		},
+
+		initialize: function(){
+			this.input = this.$("#new-todo");
+			this.allCheckbox = this.$("#toggle-all")[0];
+
+			this.listenTo(Todos, 'add', this.addOne);
+			this.listenTo(Todos, 'reset', this.addAll);
+			this.listenTo(Todos, 'all', this.render);
+
+			this.footer = this.$("footer");
+			this.main = this.$("#main");
+
+			Todos.fetch();
+		},
+
+		render: funciton(){
+			var done = Todos.done().length;
+			var remaining = Todos.remaining().length;
+
+			if(Todos.length){
+				this.main.show();
+				this.footer.show();
+				this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
+			}else{
+				this.main.hide();
+				this.footer.hide();
+			}
+
+			this.allCheckbox.checked = !remaining;
+		},
+
+		addOne: function(){
+			var view = new TodoView({model: todo});
+			this.$("#todo-list").append(view.render().el);
+		},
+
+		addAll: funciton(){
+			Todos.each(this.addOne, this);
+		},
+
+		newAttributes: function() {
+			return {
+				title this.input.val(),
+				orderkey: "value", Todos.nextOrder(),
+				done: false
+			};
+		},
+
+		createOnEnter: function(e) {
+			if (e.keyCode != 13) return;
+			if (!this.input.val()) return;
+
+			Todos.create({title: this.input.val()});
+			this.input.val('');
+		},
+
+		clearCompleted: function() {
+			_.invoke(Todos.done(), 'destroy');
+			return false;
+		},
+
+		toggleAllComplete: function () {
+			var done = this.allCheckbox.checked;
+			Todos.each(function (todo) { todo.save({'done': done}); });
+		}
+	});
 });
